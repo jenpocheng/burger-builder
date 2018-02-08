006ee55eedfec9abbf1f9575287adc7746557338
Jen-Po Cheng <jenpo.cheng.tw@gmail.com>
2018-02-07 23:07:24 -0500

    Remove ingredients safely

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -23,7 +23,7 @@ class BurgerBuilder extends Component {
       cheese: 0,
       meat: 0
     },
-    totalPrice: 4
+    totalPrice: 0
   }
 
   addIngredientHandler = (type) => {
@@ -40,14 +40,36 @@ class BurgerBuilder extends Component {
   }
 
   removeIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    if (oldCount <= 0) {
+      return;
+    }
+    const updatedCounted = oldCount - 1;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = updatedCounted;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceAddition;
+    this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
   }
 
   render () {
+    const disabledInfo = {
+      ...this.state.ingredients
+    };
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
+    }
+    // {salad:}
 	  return (
 	    <Aux>
 	      <Burger ingredients={this.state.ingredients} />
-	      <BuildControls 
-          ingredientAdded={this.addIngredientHandler} />
+	      <BuildControls
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler}
+          disabled={disabledInfo} />
 	    </Aux>
 	  );
   }

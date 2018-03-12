fffecba2de443ab5d47fb2289c78fd487df38491
Jen-Po Cheng <jenpo.cheng.tw@gmail.com>
2018-03-12 17:17:40 -0400

    308. Get a token from the backend

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import classes from './Auth.css';
@@ -11,102 +12,102 @@ class Auth extends Component {
       email: {
         elementType: 'input',
         elementConfig: {
-            type: 'email',
-            placeholder: 'Mail Address'
+          type: 'email',
+          placeholder: 'Mail Address'
         },
         value: '',
         validation: {
-            required: true,
-            isEmail: true
+          required: true,
+          isEmail: true
         },
         valid: false,
         touched: false
       },
       password: {
-          elementType: 'input',
-          elementConfig: {
-              type: 'password',
-              placeholder: 'Password'
-          },
-          value: '',
-          validation: {
-              required: true,
-              minLength: 6
-          },
-          valid: false,
-          touched: false
+        elementType: 'input',
+        elementConfig: {
+          type: 'password',
+          placeholder: 'Password'
+        },
+        value: '',
+        validation: {
+          required: true,
+          minLength: 6
+        },
+        valid: false,
+        touched: false
       }
     }
   }
 
-  checkValidity(value, rules) {
+  checkValidity ( value, rules ) {
     let isValid = true;
-    if (!rules) {
-        return true;
-    }
-    
-    if (rules.required) {
-        isValid = value.trim() !== '' && isValid;
+    if ( !rules ) {
+      return true;
     }
 
-    if (rules.minLength) {
-        isValid = value.length >= rules.minLength && isValid
+    if ( rules.required ) {
+      isValid = value.trim() !== '' && isValid;
     }
 
-    if (rules.maxLength) {
-        isValid = value.length <= rules.maxLength && isValid
+    if ( rules.minLength ) {
+      isValid = value.length >= rules.minLength && isValid
     }
 
-    if (rules.isEmail) {
-        const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-        isValid = pattern.test(value) && isValid
+    if ( rules.maxLength ) {
+      isValid = value.length <= rules.maxLength && isValid
     }
 
-    if (rules.isNumeric) {
-        const pattern = /^\d+$/;
-        isValid = pattern.test(value) && isValid
+    if ( rules.isEmail ) {
+      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      isValid = pattern.test( value ) && isValid
+    }
+
+    if ( rules.isNumeric ) {
+      const pattern = /^\d+$/;
+      isValid = pattern.test( value ) && isValid
     }
 
     return isValid;
   }
 
-  inputChangedHandler = (event, controlName) => {
+  inputChangedHandler = ( event, controlName ) => {
     const updatedControls = {
-        ...this.state.controls,
-        [controlName]: {
-            ...this.state.controls[controlName],
-            value: event.target.value,
-            valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
-            touched: true
-        }
+      ...this.state.controls,
+      [controlName]: {
+        ...this.state.controls[controlName],
+        value: event.target.value,
+        valid: this.checkValidity( event.target.value, this.state.controls[controlName].validation ),
+        touched: true
+      }
     };
-    this.setState({controls: updatedControls});
+    this.setState( { controls: updatedControls } );
   }
 
-  submitHandler = (event) => {
+  submitHandler = ( event ) => {
     event.preventDefault();
-    this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value)
+    this.props.onAuth( this.state.controls.email.value, this.state.controls.password.value);
   }
 
   render () {
     const formElementsArray = [];
     for ( let key in this.state.controls ) {
-        formElementsArray.push( {
-            id: key,
-            config: this.state.controls[key]
-        } );
+      formElementsArray.push( {
+        id: key,
+        config: this.state.controls[key]
+      } );
     }
 
     const form = formElementsArray.map( formElement => (
-        <Input
-            key={formElement.id}
-            elementType={formElement.config.elementType}
-            elementConfig={formElement.config.elementConfig}
-            value={formElement.config.value}
-            invalid={!formElement.config.valid}
-            shouldValidate={formElement.config.validation}
-            touched={formElement.config.touched}
-            changed={( event ) => this.inputChangedHandler( event, formElement.id )} />
+      <Input
+        key={formElement.id}
+        elementType={formElement.config.elementType}
+        elementConfig={formElement.config.elementConfig}
+        value={formElement.config.value}
+        invalid={!formElement.config.valid}
+        shouldValidate={formElement.config.validation}
+        touched={formElement.config.touched}
+        changed={( event ) => this.inputChangedHandler( event, formElement.id )} />
     ) );
 
     return (
@@ -122,8 +123,8 @@ class Auth extends Component {
 
 const mapDispatchToProps = dispatch => {
   return {
-    onAuth: (email, password) => dispatch(actions.auth(email, password))
+    onAuth: ( email, password) => dispatch( actions.auth( email, password) )
   };
 };
 
-export default connect(null, mapDispatchToProps)(Auth);
+export default connect( null, mapDispatchToProps )( Auth );
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -6,16 +8,17 @@ export const authStart = () => {
   };
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
   return {
     type: actionTypes.AUTH_SUCCESS,
-    authData: authData
+    idToken: token,
+    userId: userId
   };
 };
 
 export const authFail = (error) => {
   return {
-    type: actionTypes.AUTH_SUCCESS,
+    type: actionTypes.AUTH_FAIL,
     error: error
   };
 };
@@ -23,5 +26,20 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
   return dispatch => {
     dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    };
+    let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDtyau6ADtNvIb8tv26RDlLZdbpEwxHMN8';
+    axios.post(url, authData)
+      .then(response => {
+        console.log(response);
+        dispatch(authSuccess(response.data.idToken, response.data.localId));
+      })
+      .catch(err => {
+        console.log(err);
+        dispatch(authFail(err));
+      });
   };
 };
